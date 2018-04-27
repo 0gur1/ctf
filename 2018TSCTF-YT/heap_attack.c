29afff0bfaa186c6732e01ece0055797f6ba1df4
0gur1 <904466829@qq.com>
2018-04-27 11:59:40 +0800

    2018TSCTF

---
+++ 2018TSCTF-YT/heap_attack.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <unistd.h>
+char *s[12];
+char tmp[48];
+void prepare()
+{
+    setvbuf(stdin,0,2,0);
+    setvbuf(stdout,0,2,0);
+    setvbuf(stderr,0,2,0);
+}
+void cat()
+{
+    system("cat TSCTF.txt");
+}
+void inputContent(char *buf,int len)
+{
+    printf("Input content:");
+    read(0,buf,len-1);
+    buf[len-1]=0;
+}
+void newBlock()
+{
+    int size,i;
+    char *block;
+
+    printf("Input size:");
+    scanf("%d",&size);
+    getchar();
+    block = malloc(size);
+    inputContent(block,size);
+
+    for(i=0;i<9 && s[i];++i);
+    if (i==10)
+	exit(0); 
+    s[i]=block;
+    printf("Successfully add.\n");
+}
+void deleteBlock()
+{
+    int index;
+    printf("Input index:");
+    scanf("%d",&index);
+    getchar();
+    
+    if(index>=0 && index<=9)
+    {
+	free(s[index]);
+        printf("Successfully delete.\n");
+    }
+}
+void editBlock()
+{
+    int index,len;
+    printf("Input index:");
+    scanf("%d",&index);
+
+    if(index>=0 && index<=9)
+    {
+	len = strlen(s[index]);
+        inputContent(s[index],len+1);
+	inputContent(tmp,48);
+        printf("Successfully edit.\n");
+    }
+}
+int main()
+{
+    int num;
+    prepare();
+    cat();
+    while(1)
+    {
+        printf("===============menu==============\n");
+	printf("1.Add\n2.Delete\n3.Edit\n");
+        printf("Input ur choice:");
+        scanf("%d",&num);
+        getchar();
+	switch(num)
+	{
+	    case 1:
+		newBlock();
+		break;
+	    case 2:
+		deleteBlock();
+		break;
+	    case 3:
+		editBlock();
+		break;
+	}
+    }
+}
